fdd7dcffaaf6da71b39c70749574c6d9a410a773
David Eberhardt <david.eberhardt@outlook.com>
2022-06-29 00:02:39 +0100

    Update Workflow

--- packages/build/commit.ps1
+++ packages/build/commit.ps1
@@ -15,15 +15,16 @@ process {
   Write-Verbose -Message 'Creating git tag for Version, Commit and Push' -Verbose
   $CommitMessage = "Update Documentation"
   try {
+    #TODO Requires current branch, not main!
     # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
     $env:Path += ";$env:ProgramFiles\Git\cmd"
     Import-Module posh-git -ErrorAction Stop
-    git checkout main
-    git add --all
-    git status
-    git commit -s -m $CommitMessage
-    git push origin main
-    Write-Output 'Commit pushed to Origin.'
+    #git checkout main
+    #git add --all
+    #git status
+    #git commit -s -m $CommitMessage
+    #git push origin main
+    #Write-Output 'Commit pushed to Origin.'
   }
   catch {
     Write-Warning 'Creating Push to Main failed.'
